c412c66e6e65481ff9720d7e66956a5e56274388
JackTsai890405 <jack89040589@yahoo.com.tw>
2021-08-20 16:19:35 +0800

    助教批改後第一次修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,10 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  initialSlide: 1,
+  centeredSlides: true
 });

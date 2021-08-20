a3e0ddbdab83231edca7e2766baaa1c703984d42
JackTsai890405 <jack89040589@yahoo.com.tw>
2021-08-20 16:20:01 +0800

    Update 2021-08-20T08:19:59.278Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,10 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  initialSlide: 1,
+  centeredSlides: true
+});
 //# sourceMappingURL=all.js.map
